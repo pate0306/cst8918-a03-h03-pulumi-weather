1b04bdfa606dd643456d07ab69215fc52211904f
ParthB.Patel <parthb.patel@innovapost.com>
2025-01-24 16:36:11 -0500

    update index and prod file

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -42,82 +42,78 @@ const registryCredentials = containerregistry
         }
     })
 
-// Define the container image for the service.
+    // Define the container image for the service.
 const image = new docker.Image(`${prefixName}-image`, {
     imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
     build: {
-        context: appPath,
-        platform: 'linux/amd64',
+      context: appPath,
+      platform: 'linux/amd64',
     },
     registry: {
-        server: registry.loginServer,
-        username: registryCredentials.username,
-        password: registryCredentials.password,
+      server: registry.loginServer,
+      username: registryCredentials.username,
+      password: registryCredentials.password,
     },
-})
+  })
 
-// Create a container group in the Azure Container App service and make it publicly accessible.
+  // Create a container group in the Azure Container App service and make it publicly accessible.
 const containerGroup = new containerinstance.ContainerGroup(
     `${prefixName}-container-group`,
     {
-        resourceGroupName: resourceGroup.name,
-        osType: 'linux',
-        restartPolicy: 'always',
-        imageRegistryCredentials: [
-            {
-                server: registry.loginServer,
-                username: registryCredentials.username,
-                password: registryCredentials.password,
-            },
-        ],
-        containers: [
-            {
-                name: imageName,
-                image: image.imageName,
-                ports: [
-                    {
-                        port: containerPort,
-                        protocol: 'tcp',
-                    },
-                ],
-                environmentVariables: [
-                    {
-                        name: 'PORT',
-                        value: containerPort.toString(),
-                    },
-                    {
-                        name: 'WEATHER_API_KEY',
-                        value: '42618d99ad48a98de9bb0bf6ed313fa0',
-                    },
-                ],
-                resources: {
-                    requests: {
-                        cpu: cpu,
-                        memoryInGB: memory,
-                    },
-                },
-            },
-        ],
-        ipAddress: {
-            type: containerinstance.ContainerGroupIpAddressType.Public,
-            dnsNameLabel: `${imageName}`,
-            ports: [
-                {
-                    port: publicPort,
-                    protocol: 'tcp',
-                },
-            ],
+      resourceGroupName: resourceGroup.name,
+      osType: 'linux',
+      restartPolicy: 'always',
+      imageRegistryCredentials: [
+        {
+          server: registry.loginServer,
+          username: registryCredentials.username,
+          password: registryCredentials.password,
         },
+      ],
+      containers: [
+        {
+          name: imageName,
+          image: image.imageName,
+          ports: [
+            {
+              port: containerPort,
+              protocol: 'tcp',
+            },
+          ],
+          environmentVariables: [
+            {
+              name: 'PORT',
+              value: containerPort.toString(),
+            },
+            {
+              name: 'WEATHER_API_KEY',
+              value: '42618d99ad48a98de9bb0bf6ed313fa0',
+            },
+          ],
+          resources: {
+            requests: {
+              cpu: cpu,
+              memoryInGB: memory,
+            },
+          },
+        },
+      ],
+      ipAddress: {
+        type: containerinstance.ContainerGroupIpAddressType.Public,
+        dnsNameLabel: `${imageName}`,
+        ports: [
+          {
+            port: publicPort,
+            protocol: 'tcp',
+          },
+        ],
+      },
     },
-)
+  )
 
-// Export the service's IP address, hostname, and fully-qualified URL.
+  // Export the service's IP address, hostname, and fully-qualified URL.
 export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
 export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
 export const url = containerGroup.ipAddress.apply(
-    (addr) => `http://${addr!.fqdn!}:${containerPort}`,
+  (addr) => `http://${addr!.fqdn!}:${containerPort}`,
 )
-
-// It used to display acrServer & acrUsername on CLI
-// export const acrServer = registry.loginServer
-// export const acrUsername = registryCredentials.username
