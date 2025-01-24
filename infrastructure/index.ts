ead6b5140a11b7d136f446e46ff81c95d312742d
Jimish Gajjar Alg <gajj0008@algonquinlive.com>
2025-01-24 15:01:27 -0500

    Config Update

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,6 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as docker from '@pulumi/docker'
 import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
+import * as containerinstance from '@pulumi/azure-native/containerinstance'
 
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config()
@@ -20,22 +22,102 @@ const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`)
 
 // Create the container registry.
 const registry = new containerregistry.Registry(`${prefixName}ACR`, {
-  resourceGroupName: resourceGroup.name,
-  adminUserEnabled: true,
-  sku: {
-    name: containerregistry.SkuName.Basic,
-  },
+    resourceGroupName: resourceGroup.name,
+    adminUserEnabled: true,
+    sku: {
+        name: containerregistry.SkuName.Basic,
+    },
 })
 
 // Get the authentication credentials for the container registry.
 const registryCredentials = containerregistry
-  .listRegistryCredentialsOutput({
-    resourceGroupName: resourceGroup.name,
-    registryName: registry.name,
-  })
-  .apply((creds: any) => {
-    return {
-      username: creds.username!,
-      password: creds.passwords![0].value!,
-    }
-  })
+    .listRegistryCredentialsOutput({
+        resourceGroupName: resourceGroup.name,
+        registryName: registry.name,
+    })
+    .apply((creds: any) => {
+        return {
+            username: creds.username!,
+            password: creds.passwords![0].value!,
+        }
+    })
+
+// Define the container image for the service.
+const image = new docker.Image(`${prefixName}-image`, {
+    imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
+    build: {
+        context: appPath,
+        platform: 'linux/amd64',
+    },
+    registry: {
+        server: registry.loginServer,
+        username: registryCredentials.username,
+        password: registryCredentials.password,
+    },
+})
+
+// Create a container group in the Azure Container App service and make it publicly accessible.
+const containerGroup = new containerinstance.ContainerGroup(
+    `${prefixName}-container-group`,
+    {
+        resourceGroupName: resourceGroup.name,
+        osType: 'linux',
+        restartPolicy: 'always',
+        imageRegistryCredentials: [
+            {
+                server: registry.loginServer,
+                username: registryCredentials.username,
+                password: registryCredentials.password,
+            },
+        ],
+        containers: [
+            {
+                name: imageName,
+                image: image.imageName,
+                ports: [
+                    {
+                        port: containerPort,
+                        protocol: 'tcp',
+                    },
+                ],
+                environmentVariables: [
+                    {
+                        name: 'PORT',
+                        value: containerPort.toString(),
+                    },
+                    {
+                        name: 'WEATHER_API_KEY',
+                        value: '42618d99ad48a98de9bb0bf6ed313fa0',
+                    },
+                ],
+                resources: {
+                    requests: {
+                        cpu: cpu,
+                        memoryInGB: memory,
+                    },
+                },
+            },
+        ],
+        ipAddress: {
+            type: containerinstance.ContainerGroupIpAddressType.Public,
+            dnsNameLabel: `${imageName}`,
+            ports: [
+                {
+                    port: publicPort,
+                    protocol: 'tcp',
+                },
+            ],
+        },
+    },
+)
+
+// Export the service's IP address, hostname, and fully-qualified URL.
+export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
+export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
+export const url = containerGroup.ipAddress.apply(
+    (addr) => `http://${addr!.fqdn!}:${containerPort}`,
+)
+
+// It used to display acrServer & acrUsername on CLI
+// export const acrServer = registry.loginServer
+// export const acrUsername = registryCredentials.username
